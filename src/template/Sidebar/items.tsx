3d45d7c9546f8f5288632b4e044b92c7b36e1c68
adleron0 <adleron0@gmail.com>
2025-08-31 21:30:47 -0300

    oculta modulos nao finalizados

--- src/template/Sidebar/items.tsx
+++ src/template/Sidebar/items.tsx
@@ -4,56 +4,56 @@ export const items = [
     icon:  "rocket",
     path: "/home",
   },
-  {
-    title: "Clientes",
-    icon: "heart-handshake",
-    path: "/clientes",
-    isProduct: "clientes",
-    ability: "clientes",
-  },
-  {
-    title: "Comercial",
-    icon: "briefcase-business",
-    path: "/comercial",
-    isProduct: "comercial",
-    ability: "comercial",
-    subitems: [
-      {
-        title: "Orçamentos",
-        icon: "file-text",
-        path: "/comercial/orcamentos",
-      },
-      {
-        title: "Comissões",
-        icon: "hand-coins",
-        path: "/comercial/comissaoes",
-      },
-      {
-        title: "Relatórios",
-        icon: "chart-line",
-        path: "/comercial/relatorios",
-      },
-    ],
-  },
-  {
-    title: "Serviços",
-    icon: "drill",
-    path: "/servicos",
-    isProduct: "servicos",
-    ability: "servicos",
-    subitems: [
-      {
-        title: "Ordens de Serviço",
-        icon: "scroll-text",
-        path: "/servicos/ordens-de-servico",
-      },
-      {
-        title: "RDO",
-        icon: "calendar-days",
-        path: "/servicos/diario-de-obra",
-      },
-    ],
-  },
+  // {
+  //   title: "Clientes",
+  //   icon: "heart-handshake",
+  //   path: "/clientes",
+  //   isProduct: "clientes",
+  //   ability: "clientes",
+  // },
+  // {
+  //   title: "Comercial",
+  //   icon: "briefcase-business",
+  //   path: "/comercial",
+  //   isProduct: "comercial",
+  //   ability: "comercial",
+  //   subitems: [
+  //     {
+  //       title: "Orçamentos",
+  //       icon: "file-text",
+  //       path: "/comercial/orcamentos",
+  //     },
+  //     {
+  //       title: "Comissões",
+  //       icon: "hand-coins",
+  //       path: "/comercial/comissaoes",
+  //     },
+  //     {
+  //       title: "Relatórios",
+  //       icon: "chart-line",
+  //       path: "/comercial/relatorios",
+  //     },
+  //   ],
+  // },
+  // {
+  //   title: "Serviços",
+  //   icon: "drill",
+  //   path: "/servicos",
+  //   isProduct: "servicos",
+  //   ability: "servicos",
+  //   subitems: [
+  //     {
+  //       title: "Ordens de Serviço",
+  //       icon: "scroll-text",
+  //       path: "/servicos/ordens-de-servico",
+  //     },
+  //     {
+  //       title: "RDO",
+  //       icon: "calendar-days",
+  //       path: "/servicos/diario-de-obra",
+  //     },
+  //   ],
+  // },
   {
     title: "Treinamentos",
     icon: "hard-hat",
@@ -105,42 +105,42 @@ export const items = [
       },
     ],
   },
-  {
-    title: "Inventarios",
-    icon: "vault",
-    path: "/inventarios",
-    isProduct: "inventarios",
-    ability: "inventarios",
-    subitems: [
-      {
-        title: "Áreas",
-        icon: "map",
-        path: "/inventarios/areas",
-      },
-      {
-        title: "Dashboard",
-        icon: "chart-column-big",
-        path: "/inventarios/dashboard",
-      },
-      {
-        title: "Acontrole de Acesso",
-        icon: "vault",
-        path: "/inventarios/access-control",
-      },
-      {
-        title: "Autorizados",
-        icon: "Id-card",
-        path: "/inventarios/autorizados",
-      },
-    ],
-  },
-  {
-    title: "Almoxarifado",
-    icon: "warehouse",
-    path: "/almox",
-    isProduct: "almox",
-    ability: "almox",
-  },
+  // {
+  //   title: "Inventarios",
+  //   icon: "vault",
+  //   path: "/inventarios",
+  //   isProduct: "inventarios",
+  //   ability: "inventarios",
+  //   subitems: [
+  //     {
+  //       title: "Áreas",
+  //       icon: "map",
+  //       path: "/inventarios/areas",
+  //     },
+  //     {
+  //       title: "Dashboard",
+  //       icon: "chart-column-big",
+  //       path: "/inventarios/dashboard",
+  //     },
+  //     {
+  //       title: "Acontrole de Acesso",
+  //       icon: "vault",
+  //       path: "/inventarios/access-control",
+  //     },
+  //     {
+  //       title: "Autorizados",
+  //       icon: "Id-card",
+  //       path: "/inventarios/autorizados",
+  //     },
+  //   ],
+  // },
+  // {
+  //   title: "Almoxarifado",
+  //   icon: "warehouse",
+  //   path: "/almox",
+  //   isProduct: "almox",
+  //   ability: "almox",
+  // },
   {
     title: "Financeiro",
     icon: "circle-dollar-sign",
@@ -148,26 +148,26 @@ export const items = [
     isProduct: "financeiro",
     ability: "financeiro",
     subitems: [
-      {
-        title: "Contas a Pagar",
-        icon: "trending-down",
-        path: "/financeiro/pagar",
-      },
-      {
-        title: "Contas a Receber",
-        icon: "trending-up",
-        path: "/financeiro/receber",
-      },
-      {
-        title: "Notas Fiscais",
-        icon: "receipt",
-        path: "/financeiro/notas-fiscais",
-      },
-      {
-        title: "Relatórios",
-        icon: "chart-pie",
-        path: "/financeiro/relatorios",
-      },
+      // {
+      //   title: "Contas a Pagar",
+      //   icon: "trending-down",
+      //   path: "/financeiro/pagar",
+      // },
+      // {
+      //   title: "Contas a Receber",
+      //   icon: "trending-up",
+      //   path: "/financeiro/receber",
+      // },
+      // {
+      //   title: "Notas Fiscais",
+      //   icon: "receipt",
+      //   path: "/financeiro/notas-fiscais",
+      // },
+      // {
+      //   title: "Relatórios",
+      //   icon: "chart-pie",
+      //   path: "/financeiro/relatorios",
+      // },
       {
         title: "GateWays",
         icon: "wallet",

733dfa292c43393eef496f0c7fb73b8816c15e7a
adleron0 <adleron0@gmail.com>
2025-09-07 10:28:20 -0300

    save

--- src/pages/_index/index.tsx
+++ src/pages/_index/index.tsx
@@ -1,5 +1,5 @@
-import { createFileRoute, redirect } from '@tanstack/react-router'
-import Loader from '@/components/general-components/Loader';
+import { createFileRoute, redirect } from "@tanstack/react-router";
+import Loader from "@/components/general-components/Loader";
 import { useState, useEffect } from "react";
 import { useMutation } from "@tanstack/react-query";
 import { useNavigate } from "@tanstack/react-router";
@@ -7,49 +7,57 @@ import { postLogin } from "@/services/loginService";
 import { useForm, Controller } from "react-hook-form";
 import { useToast } from "@/hooks/use-toast";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Separator } from "@/components/ui/separator";
-import { EyeOpenIcon, EyeClosedIcon  } from "@radix-ui/react-icons";
+import { EyeOpenIcon, EyeClosedIcon } from "@radix-ui/react-icons";
 import { VscLoading } from "react-icons/vsc";
 import { LoginData } from "@/general-interfaces/auth.interface";
-import { useAuth } from '@/context/AuthContext';
+import { useAuth } from "@/context/AuthContext";
 import { formatCNPJ, unformatCNPJ } from "@/utils/cpnj-mask";
 import DynamicLogo from "@/components/general-components/DynamicLogo";
 import { ArrowLeft } from "lucide-react";
 
-export const Route = createFileRoute('/_index/')({
+export const Route = createFileRoute("/_index/")({
   loader: async () => {
-    const accessToken = localStorage.getItem('accessToken');
+    const accessToken = localStorage.getItem("accessToken");
     if (accessToken) {
       return redirect({
-        to: '/home',
+        to: "/home",
       });
     }
   },
-  pendingComponent: () => <Loader title={'Verificando Sessão...'}/>,
+  pendingComponent: () => <Loader title={"Verificando Sessão..."} />,
   component: Login,
 });
 
 function Login() {
-  const { setAccessTokenState, setIsLogged, reloadUser, accessToken } = useAuth();
+  const { setAccessTokenState, setIsLogged, reloadUser, accessToken } =
+    useAuth();
   const { toast } = useToast();
   const navigate = useNavigate();
   const [showPassword, setShowPassword] = useState(false);
   const { control, handleSubmit } = useForm<LoginData>({
     defaultValues: {
-      email: '',
-      password: '',
-      cnpj: ''
-    }
+      email: "",
+      password: "",
+      cnpj: "",
+    },
   });
 
   useEffect(() => {
     if (accessToken) {
       navigate({
-        to: '/home',
-      })
+        to: "/home",
+      });
       reloadUser();
     }
   }, [accessToken, navigate, reloadUser]);
@@ -67,14 +75,14 @@ function Login() {
       setIsLogged(true);
       reloadUser();
       navigate({
-        to: '/home',
-      })
+        to: "/home",
+      });
       toast({
         title: "Bem Vindo!",
         description: "Login realizado com sucesso",
         variant: "success",
-        duration: 1500
-      })
+        duration: 1500,
+      });
     },
     onError: (error: unknown) => {
       const err = error as { response?: { data?: { message?: string } } };
@@ -82,8 +90,8 @@ function Login() {
         title: "Erro ao realizar login",
         description: `${err.response?.data?.message}`,
         variant: "destructive",
-        duration: 5000
-      })
+        duration: 5000,
+      });
     },
   });
 
@@ -93,29 +101,35 @@ function Login() {
 
   return (
     <main className="h-screen flex flex-col md:flex-row w-full">
-      <div className="relative bg-muted w-full h-1/3 md:h-full flex flex-col gap-8 items-center justify-center p-4 md:p-16">
+      <div className="relative bg-muted/50 w-full h-1/3 md:h-full flex flex-col gap-8 items-center justify-center p-4 md:p-16">
         {/* Botão de Voltar para Home */}
-        <Button
+        {/*<Button
           variant="default"
-          onClick={() => navigate({ to: '/' })}
+          onClick={() => navigate({ to: "/" })}
           className="absolute top-4 left-4 bg-transparent hover:bg-primary-light border border-primary text-primary p-2 hover:scale-110 hover:text-white rounded-md transition-colors"
         >
           <ArrowLeft className="w-5 h-5" />
-        </Button>
-        
-        <div className="hidden md:flex w-full max-w-3xl">
-          <div className="flex gap-3 items-center justify-center w-full">
-            <DynamicLogo width={220} height={80} />
+        </Button>*/}
+        <div className="bg-muted md:bg-transparent p-4 rounded-md">
+          <div className="flex w-full max-w-3xl">
+            <div className="flex gap-3 items-center justify-center w-full">
+              <DynamicLogo width={220} height={80} />
+            </div>
           </div>
-        </div>
-        <div className="flex flex-col gap-4 items-center justify-center w-full max-w-3xl">
-          <h1 className="text-2xl md:text-3xl font-bold text-primary">Bem-vindo de volta!</h1>
-          <p className="text-foreground text-sm max-w-4/5 md:text-base mt-2">
-            Aqui você acessa o sistema de gerenciamento de turmas e gestão de treinamentos.
-          </p>
-          <p className="text-muted-foreground font-light max-w-4/5 text-sm md:text-base mt-2">
-            Se ainda não possui acesso ao sistema, entre em contato com seu superior ou suporte para avaliarmos seus caso e liberação do acesso.
-          </p>
+          <div className="flex flex-col gap-4 items-center justify-center w-full max-w-3xl">
+            <h1 className="text-2xl md:text-3xl font-bold text-primary">
+              Bem-vindo de volta!
+            </h1>
+            <p className="text-foreground text-sm max-w-4/5 md:text-base mt-2">
+              Aqui você acessa o sistema de gerenciamento de turmas e gestão de
+              treinamentos.
+            </p>
+            <p className="hidden md:block text-muted-foreground font-light max-w-4/5 text-sm md:text-base mt-2">
+              Se ainda não possui acesso ao sistema, entre em contato com seu
+              superior ou suporte para avaliarmos seus caso e liberação do
+              acesso.
+            </p>
+          </div>
         </div>
       </div>
       <section className="flex items-center justify-center bg-background h-full max-w-3xl w-full p-4">
@@ -136,10 +150,10 @@ function Login() {
                   name="email"
                   control={control}
                   render={({ field }) => (
-                    <Input 
-                      id="email" 
-                      placeholder="exemple@email.com" 
-                      type="email" 
+                    <Input
+                      id="email"
+                      placeholder="exemple@email.com"
+                      type="email"
                       {...field}
                     />
                   )}
@@ -166,7 +180,11 @@ function Login() {
                   className="absolute inset-y-6 right-0 p-2 h-9 flex items-center border border-gray-200 bg-primary-foreground"
                   onClick={() => setShowPassword(!showPassword)}
                 >
-                  {showPassword ? <EyeOpenIcon className="w-4 h-4" /> : <EyeClosedIcon className="w-4 h-4" />}
+                  {showPassword ? (
+                    <EyeOpenIcon className="w-4 h-4" />
+                  ) : (
+                    <EyeClosedIcon className="w-4 h-4" />
+                  )}
                 </Button>
               </div>
               <div className="mt-1">
@@ -179,13 +197,19 @@ function Login() {
                       id="cnpj"
                       placeholder="00.000.000/0000-00"
                       type="text"
-                      value={formatCNPJ(field.value || '')} // Exibe o CNPJ formatado
-                      onChange={(e) => field.onChange(unformatCNPJ(e.target.value))} // Envia o valor sem formatação
+                      value={formatCNPJ(field.value || "")} // Exibe o CNPJ formatado
+                      onChange={(e) =>
+                        field.onChange(unformatCNPJ(e.target.value))
+                      } // Envia o valor sem formatação
                     />
                   )}
                 />
               </div>
-              <Button disabled={isPending} type="submit" className="mt-4 w-full">
+              <Button
+                disabled={isPending}
+                type="submit"
+                className="mt-4 w-full"
+              >
                 {isPending ? <VscLoading className="animate-spin" /> : "Entrar"}
               </Button>
               <div className="flex items-center gap-6 mt-4">
@@ -193,14 +217,19 @@ function Login() {
                 <span className="text-xs text-muted-foreground">OU</span>
                 <Separator />
               </div>
-              <Button variant="outline" className="mt-4 w-full">Suporte</Button>
+              <Button variant="outline" className="mt-4 w-full">
+                Suporte
+              </Button>
             </CardContent>
           </form>
           <CardFooter>
-            <p className="text-muted-foreground text-center text-xs md:text-sm">Ao entrar em nossa plataforma você concorda com nossos Termos de Uso e Política de Privacidade.</p>
+            <p className="text-muted-foreground text-center text-xs md:text-sm">
+              Ao entrar em nossa plataforma você concorda com nossos Termos de
+              Uso e Política de Privacidade.
+            </p>
           </CardFooter>
         </Card>
       </section>
     </main>
   );
-};
+}
